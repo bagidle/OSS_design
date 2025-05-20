b32931b0d02c726f67cb883ca9e54259c45ae7a3
YeRa Jung <baby4226092@gmail.com>
2025-05-20 21:58:33 +0900

    Third commit: Add InputValidator

---
+++ src/InputValidator.java
@@ -0,0 +1,10 @@
+public class InputValidator {
+    public static boolean validCredit(int c) {
+        return c == 1 || c == 2 || c == 3;
+    }
+
+    public static boolean validCount(int count) {
+        return count >= 0;
+    }
+}
+
